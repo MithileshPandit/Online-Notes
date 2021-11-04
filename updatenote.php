b3372c5a4251a8daab0aa09079ec21c6f73b1366
MithileshPandit <59164318+MithileshPandit@users.noreply.github.com>
2021-11-04 13:09:14 +0530

    Add files via upload

---
+++ updatenote.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+include('connection.php');
+
+$id = $_POST['id'];
+$note = $_POST['note'];
+$time = time();
+//run a query to update the note
+$sql = "UPDATE notes SET note='$note', time = '$time' WHERE id='$id'";
+$result = mysqli_query($link, $sql);
+if(!$result){
+    echo 'error';   
+}
+?>
